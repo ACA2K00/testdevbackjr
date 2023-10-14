928f0880033ad89d643613b71e8d5a94ed42f4ed
Alejandro Castro <a01636235@tec.mx>
2023-10-13 22:52:24 -0600

    app-sql connection attempt

---
+++ Prueba_2/Prueba2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Prueba2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting Connection ...");
+
+            var datasource = @"127.0.0.1\NuxibaTest";//your server
+            var database = "oficina"; //your database name
+            var username = "root"; //username of server to connect
+            var password = "Megaman10*"; //password
+
+            //your connection string 
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            //create instanace of database connection
+            SqlConnection conn = new SqlConnection(connString);
+
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
